56fbdb1654dea989ecd46c76c099b6ba6f6effe6
canda <cpolisi6526@bths.edu>
2019-04-04 21:06:42 -0400

    absent

--- src/Digits.java
+++ src/Digits.java
@@ -8,12 +8,30 @@ public class Digits
 
     //Constructs a Digits object that
     //represents num
-    public Digits (int num)
-    {
+    public Digits (int num) {
         //implemented in part (a)
+        digitList = new ArrayList<Integer>();
+        if (num > 0) {
+            int remainder = 0;
+            while (num > 0) {
+                remainder = num % 10;
+                num = num / 10;
+                digitList.add(0, remainder);
+            }
+        }
+        else{
+            digitList.add(0);
+        }
     }
+        //implemented in part (a)
     public boolean isStrictlyIncreasing()
     {
+        for(int k=0;k<digitList.size()-1;k++){
+            if(k>k+1){
+                return false;
+            }
+        }
+        return true;
         //implemented in part (b)
     }
     //Extra method to make the runner easier to read
